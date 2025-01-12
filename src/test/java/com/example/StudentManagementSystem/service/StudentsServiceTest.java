522a1aa0c54095ecbf4d7b26dc6167a71fb50657
AnandManit09 <anand8manit@gmail.com>
2025-01-12 18:38:59 +0530

    First Junit Testing using Mockito and Assertions

--- src/test/java/com/example/StudentManagementSystem/service/StudentsServiceTest.java
+++ src/test/java/com/example/StudentManagementSystem/service/StudentsServiceTest.java
@@ -1,7 +1,30 @@
 package com.example.StudentManagementSystem.service;
 
+import com.example.StudentManagementSystem.repository.StudentsDal;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class StudentsServiceTest {
+
+    StudentsDal studentsDal= Mockito.mock(StudentsDal.class);
+
+    //Test cases for StudentsService
+    @Test
+    public void testIncrementService(){
+        //Arrange
+        StudentsService studentsService= new StudentsService(studentsDal);
+        int num=5;
+
+        //Act
+        int result=studentsService.increementService(num);
+
+
+        Assertions.assertEquals(result,num+1);
+
+    }
 }
